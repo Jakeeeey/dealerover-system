8ca5c4c9184f984a7ac4c878432f1d3c7239f996
Jake <jakedavedeguzman123@gmail.com>
2026-06-26 16:48:56 +0800

    Update cache.ts

--- src/modules/dealerover-system/bia/dealer/dealerover-kpi/utils/cache.ts
+++ src/modules/dealerover-system/bia/dealer/dealerover-kpi/utils/cache.ts
@@ -4,7 +4,7 @@ interface CacheEntry<T> {
 }
 
 class SimpleMemoryCache {
-  private store = new Map<string, CacheEntry<any>>();
+  private store = new Map<string, CacheEntry<unknown>>();
 
   get<T>(key: string): T | null {
     const entry = this.store.get(key);
